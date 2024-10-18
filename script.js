64b0a5225fa4ad1c9183c3b5c55ad487561b9e37
Refinah <112608401+Refinah@users.noreply.github.com>
2024-10-18 23:17:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function updateTime() {
+    const timeElement = document.getElementById("time");
+    const now = new Date();
+    timeElement.textContent = now.toLocaleTimeString();
+}
+
+setInterval(updateTime, 1000);
